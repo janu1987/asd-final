b16ea33ce61aa4a9ce7d8ae681407bb6de0d550c
janu1987 <102685159+janu1987@users.noreply.github.com>
2025-08-05 11:26:20 +0800

    Update download_wandb_weights.py

--- scripts/download_wandb_weights.py
+++ scripts/download_wandb_weights.py
@@ -15,6 +15,6 @@ def download_weights(model_url: str, out_path: Path):
 
 
 if __name__ == "__main__":
-    model_url = 'janu1987/3objs/run_w9r9wfq2_model:v0'
+    model_url = 'janu1987/nn/3objs/run_w9r9wfq2_model:v0'
     out_path = 'nn/3objs/weights'
     download_weights(model_url, out_path)
